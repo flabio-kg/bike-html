7636e2e853031c6cfe8672abc6422803670df9ad
ArsenTemirbekov <arsen.t@flabio.ru>
2017-04-24 01:08:57 +0600

    aaa

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -175,12 +175,31 @@ $(document).ready(function() {
 
 	});
 
-	$('.third').owlCarousel({
+	$('.js-third').owlCarousel({
 		items: 1,
 		loop: true,
 		dots: true,
 		nav: true,
 		navText:['<i class="icon-arrow-left"></i>','<i class="icon-arrow-right"></i>'],
+	});
+
+	$('.third').owlCarousel({
+		items: 2,
+		margin: 25,
+		dots: true,
+		nav: true,
+		navText:['<i class="fa fa-long-arrow-left"></i>','<i class="fa fa-long-arrow-right"></i>'],
+		responsive:{
+			0:{
+				items:1
+			},
+			600:{
+				items:1
+			},
+			1000:{
+				items:2
+			}
+		}
 	});
 
 	$('.forth').owlCarousel({
